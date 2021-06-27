8b3e20087e95789607fcece07188d1d69e7122ee
IshaanKesarwani <75419419+IshaanXCoder@users.noreply.github.com>
2021-06-27 19:19:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var dest = new Date("august 5, 2021 00:00:00").getTime();
+var dest = new Date("june 28, 2021 00:00:00").getTime();
       var x= setInterval(function(){
       var now = new Date().getTime();
       var diff= dest - now;
